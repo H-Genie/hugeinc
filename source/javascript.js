dd1cbc0ef98349d52b2a75f9c15ce76346ee8f72
H-Genie <61546294+H-Genie@users.noreply.github.com>
2020-05-06 02:09:13 +0900

    Add files via upload

--- source/javascript.js
+++ source/javascript.js
@@ -4,13 +4,15 @@ function logoClick () {
     if (header_state==0) {
         document.getElementById("section_header").style="height:440px;";
         document.getElementById("main_extend").style="padding-top:440px";        
+        document.getElementById("logo_wrap").style="background-color:#f4f4f4";
         document.getElementById("logo_wrap").firstElementChild.src="home/img/huge.svg";
         document.getElementById("logo_wrap").firstElementChild.style="width:60px";
         header_state=1;
     }
     else {
         document.getElementById("section_header").style="height:0px;";
-        document.getElementById("main_extend").style="padding-top:0px";        
+        document.getElementById("main_extend").style="padding-top:0px";    
+        document.getElementById("logo_wrap").style="background-color:#fff";    
         document.getElementById("logo_wrap").firstElementChild.src="home/img/h.svg";
         document.getElementById("logo_wrap").firstElementChild.style="width:20px";
         header_state=0;
