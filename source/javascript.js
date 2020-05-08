5f9cdae50cb3f53dd69ccfccbc4648ab24505cce
H-Genie <61546294+H-Genie@users.noreply.github.com>
2020-05-08 19:08:22 +0900

    Add files via upload

--- source/javascript.js
+++ source/javascript.js
@@ -1,20 +1,47 @@
 var header_state=0;
 
 function logoClick () {
-    if (header_state==0) {
-        document.getElementById("section_header").style="height:440px;";
-        document.getElementById("main_extend").style="padding-top:440px";        
-        document.getElementById("logo_wrap").style="background-color:#f4f4f4";
-        document.getElementById("logo_wrap").firstElementChild.src="home/img/huge.svg";
-        document.getElementById("logo_wrap").firstElementChild.style="width:60px";
-        header_state=1;
+    if (document.body.clientWidth>767) {
+
+        if (header_state==0) {
+            document.getElementById("section_header").style="height:440px;";
+            document.getElementById("main_extend").style="padding-top:440px";
+            document.getElementById("logo_wrap2").style="display:flex;";
+            document.getElementById("logo_wrap").style="display:none;";
+            header_state=1;
+        }
     }
-    else {
-        document.getElementById("section_header").style="height:0px;";
-        document.getElementById("main_extend").style="padding-top:0px";    
-        document.getElementById("logo_wrap").style="background-color:#fff";    
-        document.getElementById("logo_wrap").firstElementChild.src="home/img/h.svg";
-        document.getElementById("logo_wrap").firstElementChild.style="width:20px";
-        header_state=0;
+
+        else {
+            if (header_state==0) {
+                document.getElementById("section_header").style="height:65vh";
+                document.getElementById("main_extend").style="padding-top:440px";
+                document.getElementById("logo_wrap2").style="display:flex;";
+                document.getElementById("logo_wrap").style="display:none;";
+                header_state=1;
+            }
+        }
     }
-} 
+
+function logo2Click () {
+    if (document.body.clientWidth>767) {
+
+        if (header_state==1) {
+            document.getElementById("section_header").style="height:0px;";
+            document.getElementById("main_extend").style="padding-top:0px";
+            document.getElementById("logo_wrap").style="display:flex;";
+            document.getElementById("logo_wrap2").style="display:none;";
+            header_state=0;
+        }
+    }
+
+        else {
+            if (header_state==1) {
+                document.getElementById("section_header").style="height:0px;";
+            document.getElementById("main_extend").style="padding-top:0px";
+            document.getElementById("logo_wrap").style="display:flex;";
+            document.getElementById("logo_wrap2").style="display:none;";
+            header_state=0;
+            }
+        }
+    }
