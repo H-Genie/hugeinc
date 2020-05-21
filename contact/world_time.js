dbd3fa08533c04ef3a0c33b0c044af6c5fec3b72
H-Genie <61546294+H-Genie@users.noreply.github.com>
2020-05-21 14:13:02 +0900

    Add files via upload

--- contact/world_time.js
+++ contact/world_time.js
@@ -1,44 +1,41 @@
-var date=new Date();
-var utcHour=date.getUTCHours();
-var utcMin=date.getUTCMinutes();
-var timeZone=[-4,-5,-4,-5,-4,-4,1,-7,-5,-7,8,-4,9]
-
-//24시간 보정 함수
-function hourAdjustment(a) {
-    a=utcHour+a;
-    if(a>24) {a-=24;} else if(a<0) {a+=24;}    
-    return a;
-}
-
-//오전오후 판단 함수
-function meridiem(a) { 
-    if(a>=12) {b="PM"} else if(a==24 || a==0) {b="AM"} else if(a<12) {b="AM"}    
-    return b;
-}
-
-//분 보정 함수
-function minDigit(a) {
-    if(a<10) {a="0"+a;}
-    return a;
-}
-
-//12시간 보정 함수
-function hourAdjustment12(a) {  
-    if(a>12) {a-=12;} else if(a==24 || a==0) {a=12;} else if(a<=12) {a=a;}    
-    return a;
-}
-
-
 function hugeTime() {
     var date=new Date();
     var utcHour=date.getUTCHours();
     var utcMin=date.getUTCMinutes();
     var timeZone=[-4,-5,-4,-5,-4,-4,1,-7,-5,-7,8,-4,9]
 
-    for (i=0; i<13; i++) {
-        document.getElementsByClassName('time')[i].innerHTML=hourAdjustment12(hourAdjustment(timeZone[i]))+" : "+minDigit(utcMin)+" "+meridiem(hourAdjustment(timeZone[i]));
+        //24시간 보정 함수
+        function hourAdjustment(a) {
+            a=utcHour+a;
+            if(a>24) {a-=24;} else if(a<0) {a+=24;}    
+            return a;
+        }
 
-        if(5<=hourAdjustment(timeZone[i]) && hourAdjustment(timeZone[i])<=18) {
+        //오전오후 판단 함수
+        function meridiem(a) { 
+            if(a>=12) {b="PM"} else if(a==24 || a==0) {b="AM"} else if(a<12) {b="AM"}    
+            return b;
+        }
+
+        //분 보정 함수
+        function minDigit(a) {
+            if(a<10) {a="0"+a;}
+            return a;
+        }
+
+        //12시간 보정 함수
+        function hourAdjustment12(a) {  
+            if(a>12) {a-=12;} else if(a==24 || a==0) {a=12;} else if(a<=12) {a=a;}    
+            return a;
+        }
+
+    for (i=0; i<13; i++) {
+        document.getElementsByClassName('hour')[i].innerHTML=hourAdjustment12(hourAdjustment(timeZone[i]));
+        document.getElementsByClassName('minutes')[i].innerHTML=minDigit(utcMin);
+        document.getElementsByClassName('meridiem')[i].innerHTML=meridiem(hourAdjustment(timeZone[i]));
+        
+
+        if(6<=hourAdjustment(timeZone[i]) && hourAdjustment(timeZone[i])<=18) {
             document.getElementsByClassName("office_card")[i].style="color:black; background-color:#fff;";
             document.getElementsByClassName("sign")[i].src="contact/img/am.svg";       
         }
@@ -59,7 +56,7 @@ function updateTime() {
     setTimeout(oneMinutes,remainSeconds);
 
         function oneMinutes() {
-        setInterval(hugeTime,60000);
+        setInterval(hugeTime,6000);
          }
 }
 
