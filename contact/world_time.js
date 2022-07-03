61da67821eeda347470a8c468128a2d12090c300
h-genie <ses2000hj@hanmail.net>
2022-07-03 21:43:41 +0900

    initial commit

---
+++ contact/world_time.js
@@ -0,0 +1,63 @@
+function hugeTime() {
+    var date=new Date();
+    var utcHour=date.getUTCHours();
+    var utcMin=date.getUTCMinutes();
+    var timeZone=[-4,-5,-4,-5,-4,-4,1,-7,-5,-7,8,-4,9]
+
+        //24시간 보정 함수
+        function hourAdjustment(a) {
+            a=utcHour+a;
+            if(a>24) {a-=24;} else if(a<0) {a+=24;}    
+            return a;
+        }
+
+        //오전오후 판단 함수
+        function meridiem(a) { 
+            if(12<=a && a<=23) {b="PM"} else if(a==24) {b="AM"} else if(a<12) {b="AM"}  
+            return b;
+        }
+
+        //분 보정 함수
+        function minDigit(a) {
+            if(a<10) {a="0"+a;}
+            return a;
+        }
+
+        //12시간 보정 함수
+        function hourAdjustment12(a) {  
+            if(a>12) {a-=12;} else if(a==24 || a==0) {a=12;} else if(a<=12) {a=a;}    
+            return a;
+        }
+
+    for (i=0; i<13; i++) {
+        document.getElementsByClassName('hour')[i].innerHTML=hourAdjustment12(hourAdjustment(timeZone[i]));
+        document.getElementsByClassName('minutes')[i].innerHTML=minDigit(utcMin);
+        document.getElementsByClassName('meridiem')[i].innerHTML=meridiem(hourAdjustment(timeZone[i]));
+        
+
+        if(5<=hourAdjustment(timeZone[i]) && hourAdjustment(timeZone[i])<=18) {
+            document.getElementsByClassName("office_card")[i].style="color:black; background-color:#fff;";
+            document.getElementsByClassName("sign")[i].src="contact/img/am.svg";       
+        }
+        else {
+            document.getElementsByClassName("office_card")[i].style="color:white; background-color:#161616;";
+            document.getElementsByClassName("sign")[i].src="contact/img/pm.svg";
+        }
+    }
+}
+
+function updateTime() {
+    var date = new Date();            
+    var seconds=date.getSeconds();
+    var remainSeconds=60000-(seconds*1000);
+
+    hugeTime();
+    setTimeout(hugeTime, remainSeconds);
+    setTimeout(oneMinutes,remainSeconds);
+
+        function oneMinutes() {
+        setInterval(hugeTime,60000);
+         }
+}
+
+updateTime();
---
+++ source/javascript.js
@@ -0,0 +1,88 @@
+var header_state=0;
+
+function logoClick () {  
+             
+    if (document.body.clientWidth>767) {
+        document.getElementById('hover_border').classList.add('open');
+              
+        if (header_state==0) {
+                 
+            document.getElementById("section_header").style="height:440px;";
+            document.getElementById("main_extend").style="padding-top:440px";
+            document.getElementById("logo_wrap2").style="display:flex;";
+            document.getElementById("logo_wrap").style="display:none;";
+            header_state=1;                 
+        }        
+    }
+      
+    else {                
+        if (header_state==0) {
+            
+            document.getElementById("section_header").style="height:65vh";
+            document.getElementById("main_extend").style="padding-top:440px";
+            document.getElementById("logo_wrap2").style="display:flex;";
+            document.getElementById("logo_wrap").style="display:none;";
+            header_state=1;            
+        }            
+    }   
+        
+}
+
+function logo2Click () {
+       
+    if (document.body.clientWidth>767) {
+        document.getElementById('hover_border').classList.remove('open');
+        
+        if (header_state==1) {
+            document.getElementById("section_header").style="height:0px;";
+            document.getElementById("main_extend").style="padding-top:0px";
+            document.getElementById("logo_wrap").style="display:flex;";
+            document.getElementById("logo_wrap2").style="display:none;";
+            header_state=0;            
+        }        
+    }
+
+    else {            
+        if (header_state==1) {
+            
+        document.getElementById("section_header").style="height:0px;";
+        document.getElementById("main_extend").style="padding-top:0px";
+        document.getElementById("logo_wrap").style="display:flex;";
+        document.getElementById("logo_wrap2").style="display:none;";
+        header_state=0;            
+        }            
+    }      
+}
+
+
+function borderOn() {  
+    if (document.body.clientWidth>767) {
+    document.getElementById('hover_border').style="border-top:90px solid #f4f4f4; border-right:50px solid #f4f4f4; border-bottom:50px solid #f4f4f4; border-left:50px solid #f4f4f4;";
+    }
+}  
+    
+function borderOff() {
+    if (document.body.clientWidth>767) {
+    document.getElementById('hover_border').style="border-top:0px solid #f4f4f4; border-right:0px solid #f4f4f4; border-bottom:0px solid #f4f4f4; border-left: 0px solid #f4f4f4;";
+    }
+}
+
+
+var prevScroll = window.pageYOffset;
+window.onscroll = function() {
+    var currentScroll = window.pageYOffset;
+    if(currentScroll>100) {
+        if (prevScroll > currentScroll) {
+            if (document.body.clientWidth>767) {
+            document.getElementById("logo_wrap").style="top:52px; opacity:1;";
+            }
+            else {
+                document.getElementById("logo_wrap").style="top:10px; opacity:1;";
+            }
+        } 
+        else {
+            document.getElementById("logo_wrap").style="top:-74px; opacity:0;";
+        }
+        prevScroll = currentScroll;
+    }
+}
